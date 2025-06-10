7031207afcde6f2eb84aa29af6069f447df3199b
CCanUnal <celalcan.unal@student.vives.be>
2025-06-10 15:25:42 +0200

    xx

--- SEM2/Javascript/Labo11/demo/scripts/code.js
+++ SEM2/Javascript/Labo11/demo/scripts/code.js
@@ -5,15 +5,24 @@ window.addEventListener("load", () => {
 	const dragItem = document.getElementById("dragItem");
 	const dropZone = document.getElementById("dropZone");
 
+
+	//---------------------------------- DRAGSTART -----------------------------
+
 	dragItem.addEventListener("dragstart", e => {
 		//Hier bewaar je de string "dragItem"
 		// onder het MIME-type "text/plain" in het dataTransfer-object.
 		e.dataTransfer.setData("text/plain", "dragItem");
 	});
 
+
+	//---------------------------------- DRAGOVER -----------------------------
+
 	dropZone.addEventListener("dragover", e => {
 		e.preventDefault(); // zonder dit werkt drop niet
 	});
+
+
+	//---------------------------------- DROP -----------------------------
 
 	dropZone.addEventListener("drop", e => {
 		e.preventDefault();
